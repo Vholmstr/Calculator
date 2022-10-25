89eeeec20f7b82262d1f071bbc2bae5cfae24abf
Victor <holmstrom.victor@gmail.com>
2022-10-25 23:03:31 +0300

    Add eventlisteners to buttons and update page font styles and sizes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const numberButtons = document.querySelectorAll("#number-buttons-container button");
+
 function add (a, b) {
     return a + b;
 }
@@ -6,14 +8,37 @@ function subtract (a, b) {
     return a - b;
 }
 
-function mutiplication (a, b) {
+function mutiply (a, b) {
     return a * b;
 }
 
-function division (a, b) {
+function divide (a, b) {
     return a / b;
 }
 
 function factorial (a, b) {
     return a ** b;
 }
+
+function operate (operator, a, b) {
+    switch (operator) {
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "*":
+            return mutiply(a,b);
+        case "/":
+            return divide(a,b);
+        case "**":
+            return factorial(a,b);
+        default:
+            return null;
+    }
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log("click");
+    })
+})
