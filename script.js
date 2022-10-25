f0383df45bdd35f41604b2486e031e0c7bff8aa3
Victor <holmstrom.victor@gmail.com>
2022-10-25 22:40:50 +0300

    Add functions for adding, subtracting, multiplication, division and factorials

---
+++ script.js
@@ -0,0 +1,19 @@
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function mutiplication (a, b) {
+    return a * b;
+}
+
+function division (a, b) {
+    return a / b;
+}
+
+function factorial (a, b) {
+    return a ** b;
+}
